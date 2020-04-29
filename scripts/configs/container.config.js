cd26aeac8b78ddb916cbaa885ed1839bb4843cf5
iClusterDev <cluster.coredev@gmail.com>
2020-04-29 21:04:18 +0100

    map tool added to project

--- scripts/configs/container.config.js
+++ scripts/configs/container.config.js
@@ -1,31 +1,67 @@
 import css from "../../styles/components/container.scss";
 
+class Module {
+  constructor(classList = []) {
+    this.classList = Array.isArray(classList) && classList.length > 0 ? classList : [];
+  }
+
+  propertyMap() {
+    const list = this.classList;
+    return function () {
+      return list.reduce((map, name) => {
+        map[name] = {
+          attribute: name,
+          reflect: true,
+          type: Boolean,
+        };
+        return map;
+      }, {});
+    };
+  }
+
+  classMap() {
+    const list = this.classList;
+    return function () {
+      return list.reduce((map, name) => {
+        map[name] = this[name];
+        return map;
+      }, {});
+    };
+  }
+}
+
+const module = new Module(["fill-height", "fill-height", "fill-space"]);
+
+// console.log(module.classMap.call(this))();
+
 export default {
   styles: css,
-  classes: function () {
-    return {
-      "fill-height": this.fillHeight,
-      "fill-width": this.fillWidth,
-      "fill-space": this.fillSpace,
-    };
-  },
-  properties: function () {
-    return {
-      fillHeight: {
-        attribute: "fill-height",
-        reflect: true,
-        type: Boolean,
-      },
-      fillWidth: {
-        attribute: "fill-width",
-        reflect: true,
-        type: Boolean,
-      },
-      fillSpace: {
-        attribute: "fill-space",
-        reflect: true,
-        type: Boolean,
-      },
-    };
-  },
+  classes: module.classMap(),
+  properties: module.propertyMap(),
+  // classes: function () {
+  //   return {
+  //     "fill-height": this.fillHeight,
+  //     "fill-width": this.fillWidth,
+  //     "fill-space": this.fillSpace,
+  //   };
+  // },
+  // properties: function () {
+  //   return {
+  //     fillHeight: {
+  //       attribute: "fill-height",
+  //       reflect: true,
+  //       type: Boolean,
+  //     },
+  //     fillWidth: {
+  //       attribute: "fill-width",
+  //       reflect: true,
+  //       type: Boolean,
+  //     },
+  //     fillSpace: {
+  //       attribute: "fill-space",
+  //       reflect: true,
+  //       type: Boolean,
+  //     },
+  //   };
+  // },
 };
