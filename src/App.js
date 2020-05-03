fed2971e37c6239a3a9f5d3d34ca2c775dc228d2
iClusterDev <cluster.coredev@gmail.com>
2020-05-03 16:05:24 +0100

    dropped link components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import Component from "./core/Component";
 import Container from "./components/Container";
 import Filler from "./components/Filler";
-import Link from "./components/Link";
-import NavLink from "./components/NavLink";
 import MaterialIcon from "./components/MaterialIcon";
 
 export default class App extends Component {
   static get components() {
-    return [Filler, Container, Link, NavLink, MaterialIcon];
+    return [Filler, Container, MaterialIcon];
   }
 
   render() {
     return this.html`
       <c-filler>
         <c-container>
-          <h2>some content with a <a href="#" class="link">link</a></h2>
+          <h2>some content with a <a href="#" class="nav-link underline active">link</a></h2>
         </c-container>
       </c-filler>
     `;
--- src/modules/link.js
+++
@@ -1,8 +0,0 @@
-import Module from "../core/Module";
-import css from "../../styles/components/link.scss";
-
-const link = new Module(css);
-
-export default {
-  styles: link.css,
-};
